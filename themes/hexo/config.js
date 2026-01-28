bf374db9af17ffb23e3f0ad272777e33b64b1e22
dczhang <dczhang01@gmail.com>
2026-01-28 17:41:25 +0800

    有个语法错误，/theme/hexo/confg.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
-    'Hi，我是一个不爱写代码的程序员,人生三大爱好：读书、远行、退休！,欢迎来到我的博客🎉
+    'Hi，我是一个不爱写代码的程序员,人生三大爱好：读书、远行、退休！,欢迎来到我的博客🎉'
   ], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
