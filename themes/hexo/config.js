9bbc19b0e1f284b4b99ad3e69d71f3aaf82cf9a9
laoliu <dczhang01@gmail.com>
2026-01-29 22:12:39 +0800

    修改主题颜色

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -39,6 +39,6 @@ const CONFIG = {
   HEXO_WIDGET_DARK_MODE: true, // 夜间模式
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
 
-  HEXO_THEME_COLOR: '#8cd1ee' // 主题色配置（默认为 #928CEE）
+  HEXO_THEME_COLOR: '#000908' // 主题色配置（默认为 #928CEE）
 }
 export default CONFIG
