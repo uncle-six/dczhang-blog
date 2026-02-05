db297feea0262b4d4f6177ff215d4d5bbedda594
laoliu <dczhang01@gmail.com>
2026-02-05 20:06:36 +0800

    删除小红书主页展示

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -17,7 +17,7 @@ module.exports = {
   CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || 'https://www.instagram.com/oldsix01?igsh=dzR3MXB0bnQ0dnd2&utm_source=qr', // 您的instagram地址
   CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || '', // B站主页
   CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || '', // Youtube主页
-  CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || 'https://xhslink.com/m/2ihAQoyJYAp', // 小红书主页
+  CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || '', // 小红书主页
   CONTACT_ZHISHIXINGQIU: process.env.NEXT_PUBLIC_CONTACT_ZHISHIXINGQIU || '', // 知识星球
   CONTACT_WEHCHAT_PUBLIC: process.env.NEXT_PUBLIC_CONTACT_WEHCHAT_PUBLIC || '' ,// 微信公众号 格式：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=【xxxxxx】==#wechat_redirect
   CONTACT_DOUYIN:process.env.NEXT_PUBLIC_CONTACT_DOUYIN || 'https://v.douyin.com/HAfhG0WpQ8Y' // 抖音主页
