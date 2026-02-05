28e7c358bde156745669fe9dc57da93d55f210b6
laoliu <dczhang01@gmail.com>
2026-02-05 21:33:48 +0800

    更换首页左上角logo

--- themes/hexo/components/Logo.js
+++ themes/hexo/components/Logo.js
@@ -9,15 +9,40 @@ import SmartLink from '@/components/SmartLink'
  */
 const Logo = props => {
   const { siteInfo } = props
+  // return (
+  //   <SmartLink href='/' passHref legacyBehavior>
+  //     <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
+  //       <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'>
+  //         {' '}
+  //         {siteInfo?.title || siteConfig('TITLE')}
+  //       </div>
+  //     </div>
+  //   </SmartLink>
+  // )
   return (
-    <SmartLink href='/' passHref legacyBehavior>
-      <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
-        <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'>
-          {' '}
-          {siteInfo?.title || siteConfig('TITLE')}
-        </div>
+  <SmartLink href='/' passHref legacyBehavior>
+    <div className='flex items-center cursor-pointer space-x-2'>
+      {/* Light mode logo */}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src='/logo-light.png'
+        alt={siteInfo?.title || siteConfig('TITLE')}
+        className='h-7 w-7 block dark:hidden'
+      />
+
+      {/* Dark mode logo */}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src='/logo-dark.png'
+        alt={siteInfo?.title || siteConfig('TITLE')}
+        className='h-7 w-7 hidden dark:block'
+      />
+
+      <div className='font-medium text-lg rounded dark:border-white dark:text-white menu-link transform duration-200'>
+        {siteInfo?.title || siteConfig('TITLE')}
       </div>
-    </SmartLink>
+    </div>
+  </SmartLink>
   )
 }
 export default Logo
